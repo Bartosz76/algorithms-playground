524789de70342881704ec3404c9248684f775f9d
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-29 21:54:10 +0200

    The initial commit.

---
+++ src/main/java/bm/app/algorithmsplayground/AlgorithmsPlaygroundApplication.java
@@ -0,0 +1,17 @@
+package bm.app.algorithmsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsPlaygroundApplication.class, args);
+    }
+
+    /**
+     * This is a repository for various algorithms with their solutions.
+     */
+
+}
