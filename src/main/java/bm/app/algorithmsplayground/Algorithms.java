e2ff6871f89fa467212f4a6c30db06e7d3152d21
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-30 08:51:34 +0200

    Refactored the architecture of the application.

--- src/main/java/bm/app/algorithmsplayground/Algorithms.java
+++ src/main/java/bm/app/algorithmsplayground/Algorithms.java
@@ -2,7 +2,15 @@ package bm.app.algorithmsplayground;
 
 public class Algorithms {
 
+    public static void main(String[] args) {
+    }
+
+    /**
+     * This is basically an algorithm repository. :)
+     */
+
     public String reverseAString(String string) {
         return new StringBuilder(string).reverse().toString();
     }
+
 }
--- src/main/java/bm/app/algorithmsplayground/AlgorithmsPlaygroundApplication.java
+++
@@ -1,9 +0,0 @@
-package bm.app.algorithmsplayground;
-
-public class AlgorithmsPlaygroundApplication {
-
-    public static void main(String[] args) {
-    }
-
-
-}
