e3e1b8a6ac0360a7f4213a02971a5447e3d165b4
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-05-04 19:40:25 +0200

    Added an implementation of an Insertion algorithm in both its simple and improved ways.

--- src/main/java/bm/app/algorithmsplayground/SortingAlgorithms.java
+++ src/main/java/bm/app/algorithmsplayground/SortingAlgorithms.java
@@ -21,26 +21,35 @@ public class SortingAlgorithms {
      * of the initial collection each,
      * - sort the sub-collections,
      * - merge the sorted sun-collections back into a single sorted one.
+     *
+     * Mergesort is used when the data structure doesn't support random access as it works
+     * with purely sequential access (forward iterators, not random access iterators). It's used
+     * widely for an external sorting where random access can be expensive in comparison to
+     * sequential access (e.g. when the file to sort doesn't fit into memory, break it into
+     * smaller chunks that do fit and sort them independently).
+     * Mergesort is also a stable sort.
+     * It's quicker with linked lists due to the pointers being easy to change when merging
+     * collections. It only requires a single pass through the collection, so O(n).
+     * If there's a lot of parallelization, Mergesort is simpler than other algorithms.
      */
-    public Comparable[] mergeSort(Comparable[] collection) {
+    public void mergeSort(Comparable[] providedCollection) {
         // If the collection is empty - no need to do a thing.
-        if (collection.length <= 1) {
-            return collection;
+        if (providedCollection.length <= 1) {
+            return;
         }
 
         // Split the collection in half into two sub-collections.
-        Comparable[] first = new Comparable[collection.length / 2];
-        Comparable[] second = new Comparable[collection.length - first.length];
-        System.arraycopy(collection, 0, first, 0, first.length);
-        System.arraycopy(collection, first.length, second, 0, second.length);
+        Comparable[] first = new Comparable[providedCollection.length / 2];
+        Comparable[] second = new Comparable[providedCollection.length - first.length];
+        System.arraycopy(providedCollection, 0, first, 0, first.length);
+        System.arraycopy(providedCollection, first.length, second, 0, second.length);
 
         // Sort both halves recursively.
         mergeSort(first);
         mergeSort(second);
 
         // Merge both halves together, overwriting the original provided collection.
-        merge(first, second, collection);
-        return collection;
+        merge(first, second, providedCollection);
     }
 
     private void merge(Comparable[] first, Comparable[] second, Comparable[] result) {
@@ -67,5 +76,79 @@ public class SortingAlgorithms {
         // Copy the remaining elements from both halves - each half will have already sorted elements.
         System.arraycopy(first, indexFirst, result, indexMerged, first.length - indexFirst);
         System.arraycopy(second, indexSecond, result, indexMerged, second.length - indexSecond);
+    }
+
+    /**
+     * Insertion algorithm.
+     * This algorithm repeatedly takes the next element from the not-yet-sorted section and
+     * inserts it into the sorted one at the correct position.
+     * The steps of this algorithm are:
+     * - data elements are grouped into two sections: the sorted and the un-sorted one,
+     * - an element of the un-sorted one is taken,
+     * - said element is inserted into the sorted section at the position determined by
+     *   the comparable property,
+     * - rinse and repeat until the un-sorted section is emptied.
+     *
+     * This algorithm is efficient for small data inputs. It is also adaptive - it's quite
+     * efficient if the input is already substantially sorted. Moreover, it's stable - it
+     * does not change the relative order of the elements with equal keys.
+     *
+     * This is a simple version.
+     */
+    public void insertionSort(Object[] providedObject, int fromIndex, int toIndex) {
+        Object temporaryHolder;
+        for (int i = fromIndex + 1; i < toIndex; i++) {
+            temporaryHolder = providedObject[i];
+            int j = i;
+            while (j > fromIndex && ((Comparable) providedObject[j - 1]).compareTo(temporaryHolder) > 0) {
+                providedObject[j] = providedObject[j - 1];
+                j--;
+            }
+            providedObject[j] = temporaryHolder;
+        }
+    }
+    /**
+     * This is an improved version. We are employing a binary search algorithm here.
+     * It can be a better choice for collections that are already partly sorted.
+     */
+    public void insertionSortImproved(Object[] providedCollection, int fromIndex, int toIndex)
+    {
+        Object temporaryHolder;
+        for (int i = fromIndex + 1; i < toIndex; i++)
+        {
+            temporaryHolder = providedCollection[i];
+            int jLeft = fromIndex;
+            int jRight = i - 1;
+            // Check if its current position is its suitable position.
+            if (((Comparable) providedCollection[jRight]).compareTo(temporaryHolder) > 0)
+            {
+                // Perform the binary search.
+                while (jRight - jLeft >= 2)
+                {
+                    int jMiddle = (jRight - jLeft) / 2 + jLeft - 1;
+                    if (((Comparable) providedCollection[jMiddle]).compareTo(temporaryHolder) > 0) {
+                        jRight = jMiddle;
+                    } else {
+                        jLeft = jMiddle + 1;
+                    }
+                }
+                if (jRight - jLeft == 1)
+                {
+                    int jMiddle = jLeft;
+                    if (((Comparable) providedCollection[jMiddle]).compareTo(temporaryHolder) > 0) {
+                        jRight = jMiddle;
+                    } else {
+                        jLeft = jMiddle + 1;
+                    }
+                }
+                // Place the element.
+                int j = i;
+                for (j = i; j > jLeft; j--)
+                {
+                    providedCollection[j] = providedCollection[j - 1];
+                }
+                providedCollection[j] = temporaryHolder;
+            }
+        }
     }
 }
--- src/test/java/bm/app/algorithmsplayground/SortingAlgorithmsTest.java
+++ src/test/java/bm/app/algorithmsplayground/SortingAlgorithmsTest.java
@@ -1,12 +1,7 @@
 package bm.app.algorithmsplayground;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,9 +15,42 @@ class SortingAlgorithmsTest {
         Integer[] providedArray = {23, 5, 6, 21, 8, 76, 3, 21};
         Integer[] expectedResult = {3, 5, 6, 8, 21, 21, 23, 76};
         //when
-        Integer[] actualResult = (Integer[]) sortingAlgorithms.mergeSort(providedArray);
+        sortingAlgorithms.mergeSort(providedArray);
         //then
-        assertThat(actualResult).isEqualTo(expectedResult);
+        assertThat(providedArray).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldSortTheProvidedCollectionByInsertionSortSimpleWay() {
+        //given
+        Integer[] providedArray = {9, 18, 2, 3, 56, 213, 1, 0, 0};
+        Integer[] expectedResult = {0, 0, 1, 2, 3, 9, 18, 56, 213};
+        //when
+        sortingAlgorithms.insertionSort(providedArray, 0, providedArray.length);
+        //then
+        assertThat(providedArray).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldSortOnlyAPartOfTheProvidedCollectionByInsertionSortSimpleWay() {
+        //given
+        Integer[] providedArray = {287, 2, 3, 456, 2, 78};
+        Integer[] expectedArray = {287, 2, 3, 2, 78, 456};
+        //when
+        sortingAlgorithms.insertionSort(providedArray, 3, providedArray.length);
+        //then
+        assertThat(providedArray).isEqualTo(expectedArray);
+    }
+
+    @Test
+    void shouldSortTheProvidedCollectionByInsertionSortImprovedWay() {
+        //given
+        Integer[] providedArray = {3, 98, 6, 34, 235, 0, 8, 0, 433};
+        Integer[] expectedResult = {0, 0, 3, 6, 8, 34, 98, 235, 433};
+        //when
+        sortingAlgorithms.insertionSortImproved(providedArray, 0, providedArray.length);
+        //then
+        assertThat(providedArray).isEqualTo(expectedResult);
     }
 
     @BeforeEach
